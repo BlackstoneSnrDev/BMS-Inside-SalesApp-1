78afc2f8fa45c9a1941eeaafc9973f922134446f
Karla Cardenas <kcardenas@blackstonemedicalservices.com>
2022-06-09 18:02:04 -0500

    Queue design finished. Missing CRUD.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,75 +2,63 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-
 import { routing, appRoutingProviders } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
 
+///// My components 
+// HTML
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormComponent } from './components/form/form.component';
 import { LogComponent } from './components/log/log.component';
 import { PhoneComponent } from './components/phone/phone.component';
-import { TablecccComponent } from './components/table/table.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TableComponent } from './components/table/table.component';
 
-
+///// Partials
 import { QueueComponent } from './partials/queue/queue.component';
 import { CallInfoComponent } from './partials/call-information/call-information.component';
 import { SettingsComponent } from './partials/settings/settings.component';
 import { StatisticsComponent } from './partials/statistics/statistics.component';
 
+///// My directives
 import { TextareaAutoresizeDirective } from './directives/resize.directive';
 
-import { MatTableModule } from '@angular/material/table';
+///// Libraries
+// Angular material
 import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { NgChartsModule } from 'ng2-charts';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+// Ng2 Charts
+import { NgChartsModule } from 'ng2-charts';
+
+// PrimeNG
 import { TableModule } from 'primeng/table';
-import { ProductService } from './components/table/productservice';
 import { PaginatorModule } from 'primeng/paginator';
-import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
-
-
-import { ToastModule } from 'primeng/toast';
-import { CalendarModule } from 'primeng/calendar';
-import { SliderModule } from 'primeng/slider';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { ContextMenuModule } from 'primeng/contextmenu';
-import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { InputTextModule } from 'primeng/inputtext';
-import { FileUploadModule } from 'primeng/fileupload';
 import { ToolbarModule } from 'primeng/toolbar';
-import { RatingModule } from 'primeng/rating';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { InputNumberModule } from 'primeng/inputnumber';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
 import { ConfirmationService } from 'primeng/api';
 import { MessageService } from 'primeng/api';
-import { InputTextareaModule } from 'primeng/inputtextarea';
 import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
 
   declarations: [
     AppComponent,
+    ///// My components 
+    // HTML
     FormComponent,
     LogComponent,
     PhoneComponent,
     NavbarComponent,
-    TablecccComponent,
+    TableComponent,
+    // Partials
     QueueComponent,
     CallInfoComponent,
     SettingsComponent,
     StatisticsComponent,
+    ///// My directives
     TextareaAutoresizeDirective,
   ],
 
@@ -78,29 +66,30 @@ import { TooltipModule } from 'primeng/tooltip';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatTableModule,
+    ///// Libraries
+    // Angular material
     MatInputModule,
-    MatPaginatorModule,
-    MatSortModule,
     MatTooltipModule,
-    NgChartsModule,
     MatProgressSpinnerModule,
-    MatCheckboxModule,
-    FormsModule,
+    // Ng2 Charts
+    NgChartsModule,
+    // PrimeNG
     TableModule,
     PaginatorModule,
     CheckboxModule,
-    ToastModule, InputTextModule, FileUploadModule, ToolbarModule, RatingModule, RadioButtonModule, InputNumberModule,
-    ConfirmDialogModule, InputTextareaModule, CalendarModule, SliderModule, ContextMenuModule, MultiSelectModule, DialogModule, DropdownModule, ProgressBarModule, TooltipModule
-    ,
-    ButtonModule,
+    ToolbarModule,
+    ConfirmDialogModule,
+    DialogModule,
+    TooltipModule,
     routing
   ],
 
   providers: [
-    appRoutingProviders, ProductService, MessageService, ConfirmationService
+    appRoutingProviders,
+    // PrimeNG
+    ConfirmationService,
+    MessageService,
   ],
-
   bootstrap: [AppComponent]
 })
 
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,69 +1,86 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Product } from './product';
-import { ProductService } from './productservice';
+import { Component , ViewChild} from '@angular/core';
+// Get table data
+import { DataService } from '../../services/services.service';
+// PrimeNG
 import { ConfirmationService } from 'primeng/api';
 import { MessageService } from 'primeng/api';
-import { DataService } from '../../services/services.service';
+import { PrimeNGConfig } from "primeng/api";
+import {Table} from 'primeng/table';
 
 @Component({
 
-  selector: 'tableccomponent',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css',
-    '../../css/neumorphism.component.css'],
+    selector: 'table-component',
+    templateUrl: './table.component.html',
+    styleUrls: ['./table.component.css',
+        '../../css/neumorphism.component.css'],
     providers: [MessageService]
 
 })
 
-export class TablecccComponent{
+export class TableComponent {
 
-  productDialog: boolean = false;
+    
+    public thData: any;
+    public tdData: any;
+    public selectedRows: any = [];
+    public toggleMenuTable: boolean = false;
+    public toggleRowMenuTable: boolean = false;
+    public messageDanger: string = ""
 
+    constructor(private DataService: DataService, private messageService: MessageService, private confirmationService: ConfirmationService, private primengConfig: PrimeNGConfig) { }
 
-  product: Product = {};
+    ngOnInit() {
+        
+        this.primengConfig.ripple = true;
+        this.DataService.getTableData().subscribe(
+            response => {
 
-  selectedRows: any = [];
+                this.thData = response.table_th
+                this.tdData = response.table_td
 
-  submitted: boolean = false;
-  public thData: any;
-  public tdData: any;
+                var slIndex = 0
+                for (let i = 0; i < this.tdData.length; i++) {
+                    slIndex = i
+                    this.tdData[slIndex]["slIndex"] = i;
+                }
+                
+            },
+            error => {
+                console.error(error)
+            }
+        )
+    }
 
-  constructor(private DataService: DataService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
+    @ViewChild('dataTable') table!: Table;
 
-  ngOnInit() {
+    toggleMenu(){
+        this.toggleMenuTable = ! this.toggleMenuTable
+    }
+    toggleRowMenu(){
+        this.toggleRowMenuTable = ! this.toggleRowMenuTable
+    }
 
-      this.DataService.getTableData().subscribe(
-        response => {
-  
-          this.thData = response.table_th
-          this.tdData = response.table_td
+    deleteSelectedRows() {
 
-          var index = 0
-          for (let i = 0; i < this.tdData.length; i++) {
-            index = i
-            this.tdData[index]["index"] = index; 
+        if(this.tdData.length > 0){
+            this.confirmationService.confirm({
+                message: 'Are you sure you want to delete this selection?',
+                header: 'Confirm',
+                icon: 'pi pi-exclamation-triangle',
+                accept: () => {
+                    this.tdData = this.tdData.filter((val: any) => !this.selectedRows.includes(val));
+                    this.selectedRows = [];
+                    this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Rows Deleted', life: 3000 });
+                }
+            });
+        }else{
+            this.messageDanger = "Please select a row down below."
+            console.log(this.tdData)
 
         }
 
-        },
-        error => {
-          console.error(error)
-        }
-      )
-  }
-
-  deleteSelectedRows() {
-      this.confirmationService.confirm({
-          message: 'Are you sure you want to delete this selection?',
-          header: 'Confirm',
-          icon: 'pi pi-exclamation-triangle',
-          accept: () => {
-              this.tdData = this.tdData.filter((val:any) => !this.selectedRows.includes(val));
-              this.selectedRows = [];
-              this.messageService.add({severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
-          }
-      });
-  }
+        
+    }
 
 
 }
