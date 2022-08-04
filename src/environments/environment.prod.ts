cfabcc0757daa293039ae71bf7af8adc9ad6fa7b
blackstonesnrdev <rrieben@blackstonemedicalservices.com>
2022-08-04 11:02:45 -0400

    Pulling down note features

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+// import { environment as common } from './environment';
+
 export const environment = {
-  production: true
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBWe_LKvR_EOjbu6mjcDJKFYjohoabNyAM",
+      authDomain: "insidesalesapi.firebaseapp.com",
+      projectId: "insidesalesapi",
+      storageBucket: "insidesalesapi.appspot.com",
+      messagingSenderId: "491724602022",
+      appId: "1:491724602022:web:9e8365a8797cf115b31267",
+      measurementId: "G-YTTCXK536R"
+    },
 };
